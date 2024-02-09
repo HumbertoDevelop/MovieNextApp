5fe8dfc6b5e819132e4876b46cd6b87b23aeb32d
Humberto García <humbertodev14@gmail.com>
2024-02-09 16:14:22 -0400

    Fixin param on Provider component if is undefined -7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import NavbarApp from "@/components/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,7 +19,6 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<Providers>
-					<NavbarApp />
 					{children}
 				</Providers>
 			</body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 /** @format */
 
 import GenreNavbar from "@/components/GenreNavbar";
+import NavbarApp from "@/components/Navbar";
 import { NowPlaying } from "@/components/NowPlaying";
 // import { TopRatedListMovies } from "@/components/TopRatedListMovies";
 import { UpComingListMovies } from "@/components/UpComingListMovies";
@@ -8,6 +9,7 @@ import { UpComingListMovies } from "@/components/UpComingListMovies";
 export default async function Page() {
 	return (
 		<div>
+		<NavbarApp />
 			<header className="container-header grid grid-cols-1 p-2 h-screen items-center sm:grid-cols-2 lg:grid-cols-3">
 				<NowPlaying />
 			</header>
